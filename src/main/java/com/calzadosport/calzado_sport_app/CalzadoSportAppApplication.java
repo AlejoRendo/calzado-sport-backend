0318fde52b0f27c4c6f702bc36b5087551c170e6
Alejandro Rendón <jalgrisales@poligran.edu.co>
2024-11-17 14:36:19 -0500

    Proyecto creado en  Java / Spring boot 3

---
+++ src/main/java/com/calzadosport/calzado_sport_app/CalzadoSportAppApplication.java
@@ -0,0 +1,13 @@
+package com.calzadosport.calzado_sport_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalzadoSportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalzadoSportAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calzadosport/calzado_sport_app/CalzadoSportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calzadosport.calzado_sport_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalzadoSportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
